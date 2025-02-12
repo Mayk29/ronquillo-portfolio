fa5b6a54ee7bda03ac9ff131c867560d9ee12b2f
Mayk29 <144456367+Mayk29@users.noreply.github.com>
2025-02-12 23:10:26 +0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,57 +1,57 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyCAoSlg5BYN95mx3iosoA93YBqQJFecbW0",
-    authDomain: "ronquillo-contact-portfolio.firebaseapp.com",
-    databaseURL: "https://ronquillo-contact-portfolio-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "ronquillo-contact-portfolio",
-    storageBucket: "ronquillo-contact-portfolio.firebasestorage.app",
-    messagingSenderId: "995849849476",
-    appId: "1:995849849476:web:720596dd8ad353ada6d0d1"
-  };
+// const firebaseConfig = {
+//     apiKey: "AIzaSyCAoSlg5BYN95mx3iosoA93YBqQJFecbW0",
+//     authDomain: "ronquillo-contact-portfolio.firebaseapp.com",
+//     databaseURL: "https://ronquillo-contact-portfolio-default-rtdb.asia-southeast1.firebasedatabase.app",
+//     projectId: "ronquillo-contact-portfolio",
+//     storageBucket: "ronquillo-contact-portfolio.firebasestorage.app",
+//     messagingSenderId: "995849849476",
+//     appId: "1:995849849476:web:720596dd8ad353ada6d0d1"
+//   };
 
-// initialize firebase
-firebase.initializeApp(firebaseConfig);
+// // initialize firebase
+// firebase.initializeApp(firebaseConfig);
 
-// reference your database
-var contactFormDB = firebase.database().ref("contactForm");
+// // reference your database
+// var contactFormDB = firebase.database().ref("contactForm");
 
-document.getElementById("contactForm").addEventListener("submit", submitForm);
+// document.getElementById("contactForm").addEventListener("submit", submitForm);
 
-function submitForm(e) {
-  e.preventDefault();
+// function submitForm(e) {
+//   e.preventDefault();
 
-  var fname = getElementVal("fname");
-  var comp_name = getElementVal("comp_name");
-  var email = getElementVal("email");
-  var message = getElementVal("message");
+//   var fname = getElementVal("fname");
+//   var comp_name = getElementVal("comp_name");
+//   var email = getElementVal("email");
+//   var message = getElementVal("message");
 
-  saveMessages(fname, comp_name, email, message);
+//   saveMessages(fname, comp_name, email, message);
 
-  //   enable alert
-  document.querySelector(".alert").style.display = "block";
+//   //   enable alert
+//   document.querySelector(".alert").style.display = "block";
 
-  //   remove the alert
-  setTimeout(() => {
-    document.querySelector(".alert").style.display = "none";
-  }, 3000);
+//   //   remove the alert
+//   setTimeout(() => {
+//     document.querySelector(".alert").style.display = "none";
+//   }, 3000);
 
-  //   reset the form
-  document.getElementById("contactForm").reset();
-}
+//   //   reset the form
+//   document.getElementById("contactForm").reset();
+// }
 
-const saveMessages = (fname, comp_name, email, message) => {
-  var newContactForm = contactFormDB.push();
+// const saveMessages = (fname, comp_name, email, message) => {
+//   var newContactForm = contactFormDB.push();
 
-  newContactForm.set({
-    fname: fname,
-    comp_name: comp_name,
-    email: email,
-    message: message,
-  });
-};
+//   newContactForm.set({
+//     fname: fname,
+//     comp_name: comp_name,
+//     email: email,
+//     message: message,
+//   });
+// };
 
-const getElementVal = (id) => {
-  return document.getElementById(id).value;
-};
+// const getElementVal = (id) => {
+//   return document.getElementById(id).value;
+// };
 
 function hamburg(){
     const navbar = document.querySelector(".dropdown")
